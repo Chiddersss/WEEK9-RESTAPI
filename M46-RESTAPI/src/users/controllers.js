e2a85c252a2cb0bb2473121216074dc6d6cf0824
Chidders <64120293+Chiddersss@users.noreply.github.com>
2023-05-15 17:30:13 +0100

    modified:   .DS_Store 	modified:   M46-RESTAPI/.vscode/settings.json 	modified:   M46-RESTAPI/src/middleware/index.js 	modified:   M46-RESTAPI/src/users/controllers.js 	modified:   M46-RESTAPI/src/users/model.js 	modified:   M46-RESTAPI/src/users/routes.js
    	modified:   M46-RESTAPI/.gitignore

--- M46-RESTAPI/src/users/controllers.js
+++ M46-RESTAPI/src/users/controllers.js
@@ -1,107 +1,122 @@
-const User = require('./model');
-const byCrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const  User = require ("./model") 
+
+const jwt = require("jsonwebtoken")
+
+// {
+//     "username" : "Alex",
+//     "email": "alex@email.com",
+//     "password": "password123"
+//  }
 
 const registerUser = async (req, res) => {
-    try {
+    try { 
         console.log("next called and inside controller")
         // const user = await User.create({
         //     username: req.body.username,
         //     email: req.body.email,
-        //     password: req.body.password 
-        // })
-
+        //     password: req.body.password
+        // });
         const user = await User.create(req.body)
         res.status(201).json({
-            message: "User created successfully",
-            user: { username: req.body.username, email: req.body.email }
+            message: "success",
+            user: {username: req.body.username, email: req.body.email}
         })
-
-    } catch {
+    } catch (error) {
         res.status(501).json({errorMessage: error.message, error: error})
     }
 }
 
-//TODO: add the rest of the CRUD operations
-// getAllUsers
-// updateUser
-// deleteUser
 
 const getAllUsers = async (req, res) => {
     try {
-        const users = await User.findAll()
-        for (let user of users) {
-            user.password = " "
-        }
-        res.status(200).json({users: users})
-    } catch (error) {
-        res.status(501).json({errorMessage: error.message, error: error})
-    }
-}
+      const users = await User.findAll();
 
+      // remove passwords from users object
+    //   for (let user of users) {
+    //     user.password = "";
+    //   }
+
+      res.status(200).json({ message: "success", users: users });
+    } catch (error) {
+      res.status(501).json({ errorMessage: error.message, error: error });
+    }
+};
+
+// {
+//     "username" : "Alex",
+//     "updateKey" : "email",
+//     "updateValue" : "alexA@email.com"
+// }
 const updateUser = async (req, res) => {
     try {
-        const user = await User.update(req.body, {
-            where: { id: req.params.id }
-        })
-        res.status(200).json({user: user})
+      const updateResult = await User.update(
+        { [req.body.updateKey]: req.body.updateValue },
+        { where: { username: req.body.username } }
+      );
+  
+      res.status(201).json({ message: "success", updateResult: updateResult });
     } catch (error) {
-        res.status(501).json({errorMessage: error.message, error: error})
+      res.status(501).json({ errorMessage: error.message, error: error });
     }
-}
+};
 
 const deleteUser = async (req, res) => {
     try {
-        const user = await User.destroy({
-            where: { id: req.params.id }
-        })
-        res.status(200).json({user: user})
+      const result = await User.destroy({
+        where: {
+          username: req.body.username,
+        },
+      });
+      res.status(202).json({ message: "success", result });
     } catch (error) {
-        res.status(501).json({errorMessage: error.message, error: error})
+      res.status(501).json({ errorMessage: error.message, error: error });
     }
-}
+  }; 
 
+//   {
+//     "username" : "Pete",
+//     "email": "pete@email.com",
+//     "password": "password"
+//   }
 const login = async (req, res) => {
     try {
-        if(req.authUser){
-            console.log(req.authUser)
+        if (req.authUser) {
+          res.status(200).json({
+            message: "success",
+            user: {
+              username: req.authUser.username,
+              email: req.authUser.email
+            }
+          })
+          return
         }
-    } catch {
-        res.status(501).json({ errorMessage: error.message, error: error})
+
+        const token = await jwt.sign({id: req.user.id}, process.env.SECRET);
+
+        res.status(200).json({
+            message: "success",
+            user: {
+                username: req.user.username,
+                email: req.user.email,
+                token: token
+            }
+        })
+    } catch (error) {
+        res.status(501).json({ errorMessage: error.message, error: error });
     }
 }
 
-const tokenCheck = async (req, res, next) => {
-    try {
-        if (!req.header("Authorization")) {
-            throw new Error("No header or token passed in the request")
-        }
-        const token = req.header("Authorization").replace("Bearer ", " ")
-        console.log("!!!!!!!!!")
-        console.log(token)
 
-        const decodedToken = await jwt.verify(token, process.env.SECRET_KEY)
-        console.log(decodedToken)
 
-        const user = await User.findOne({where: {id: decodedToken.id}})
-        console.log(user)
-
-        if (!user) {
-            throw new Error("User not found")
-        }
-
-        req.authUser = user
-        next()
-
-    } catch {
-        res.status(501).json({ errorMessage: error.message, error: error})
-    }
-}
+//TODO: Add the rest of the CRUD operations
+// getAllUsers
+// updateUser
+// deleteUser
 
 module.exports = {
     registerUser,
     getAllUsers,
     updateUser,
     deleteUser,
-    tokenCheck   
+    login
 }
