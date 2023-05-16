f83ea2066a3fb53eb4fd5c3a306324e62e4ff9f5
Chidders <64120293+Chiddersss@users.noreply.github.com>
2023-05-16 02:17:55 +0100

    modified:   M46-RESTAPI/src/users/controllers.js 	modified:   M46-RESTAPI/src/users/routes.js

--- M46-RESTAPI/src/users/controllers.js
+++ M46-RESTAPI/src/users/controllers.js
@@ -2,20 +2,11 @@ const  User = require ("./model")
 
 const jwt = require("jsonwebtoken")
 
-// {
-//     "username" : "Alex",
-//     "email": "alex@email.com",
-//     "password": "password123"
-//  }
 
 const registerUser = async (req, res) => {
     try { 
         console.log("next called and inside controller")
-        // const user = await User.create({
-        //     username: req.body.username,
-        //     email: req.body.email,
-        //     password: req.body.password
-        // });
+  
         const user = await User.create(req.body)
         res.status(201).json({
             message: "success",
@@ -31,10 +22,6 @@ const getAllUsers = async (req, res) => {
     try {
       const users = await User.findAll();
 
-      // remove passwords from users object
-    //   for (let user of users) {
-    //     user.password = "";
-    //   }
 
       res.status(200).json({ message: "success", users: users });
     } catch (error) {
@@ -42,11 +29,6 @@ const getAllUsers = async (req, res) => {
     }
 };
 
-// {
-//     "username" : "Alex",
-//     "updateKey" : "email",
-//     "updateValue" : "alexA@email.com"
-// }
 const updateUser = async (req, res) => {
     try {
       const updateResult = await User.update(
@@ -73,11 +55,6 @@ const deleteUser = async (req, res) => {
     }
   }; 
 
-//   {
-//     "username" : "Pete",
-//     "email": "pete@email.com",
-//     "password": "password"
-//   }
 const login = async (req, res) => {
     try {
         if (req.authUser) {
@@ -105,13 +82,6 @@ const login = async (req, res) => {
         res.status(501).json({ errorMessage: error.message, error: error });
     }
 }
-
-
-
-//TODO: Add the rest of the CRUD operations
-// getAllUsers
-// updateUser
-// deleteUser
 
 module.exports = {
     registerUser,
